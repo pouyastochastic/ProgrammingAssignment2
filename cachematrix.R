cfb5d028546a12de205f19619d1d2dc742f5e2b2
pouyastochastic <30681341+pouyastochastic@users.noreply.github.com>
2017-08-20 22:15:57 -0700

    HW2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function, makeCacheMatrix creates a special "matrix"
+## The second function, cacheSolve calculates the inverse of the special "matrix" 
+## created with the makeCacheMatrix function.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates a special "matrix" which is really a list containing a function to
+##### 1) set the value of the matrix
+##### 2) get the value of the matrix
+##### 3) set the matrix inverse
+##### 4) get the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+      inv.x <- NULL
+      set <- function(y){
+            x <<- y
+            inv.x <<- NULL
+      }
+      get <- function() x
+      setinv <- function(x_inv) inv.x <<- x_inv
+      getinv <- function() inv.x
+      list(set = set, get = get, setinv = setinv, getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve first checks to see if the matrix inverse has already been calculated. 
+## If so, it gets the matrix inverse from the cache and skips the computation. 
+## Otherwise, it calculates the inverse of the matrix and sets the value of the inverse
+## in the cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      x.inv <- x$getinv()
+      if (!is.null(x.inv)){
+            message("getting cached data")
+            return(x.inv)
+      }
+      data <- x$get()
+      x.inv <- solve(data,...)
+      x$setinv(x.inv)
+      x.inv
 }
